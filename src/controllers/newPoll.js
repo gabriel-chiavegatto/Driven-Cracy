7b6d4848389577652863b917f76cc0fb6c05ce2a
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-23 03:10:34 -0300

    get and post polls

--- src/controllers/newPoll.js
+++ src/controllers/newPoll.js
@@ -1,7 +1,43 @@
-import {db, ObjectId} from "../database.js";
+import { db, ObjectId } from "../database.js";
+import baseJoi from 'joi';
+import joiDate from '@joi/date';
+const joi = baseJoi.extend(joiDate);
+import dayjs from 'dayjs';
 
-async function newPoll(req,res){
 
-    res.sendStatus(201);
+async function newPoll(req, res) {
+    try {
+        const poll = req.body;
+
+        const pollSchema = joi.object({
+            title: joi.string().required(),
+            expireAt: joi.date().format('YYYY-MM-DD').utc()
+        })
+        const validation = pollSchema.validate(poll);
+        if (validation.error) {
+            return res.status(422).send("Schema error")
+        }
+        if (!poll.expireAt) {
+            const day = dayjs().format("DD");
+            let month = parseInt(dayjs().format("MM"));
+            let year = parseInt(dayjs().format("YYYY"));
+            if (month == "12") {
+                month = "01";
+                const aux = parseInt(year) + 1;
+                year = aux.toString();
+            } else {
+                if(month == '09'){
+                    month = "10"
+                } else{
+                    const aux = parseInt(month[1]) + 1;
+                    month[1] = aux.toString()
+                }
+            }
+            poll.expireAt = year + "-" + month + "-" + day;
+        }
+
+        await db.collection("polls").insertOne(poll);
+        res.sendStatus(201);
+    } catch (error) { console.log(error); res.sendStatus(422) }
 }
-export {newPoll}
+export { newPoll }
--- src/controllers/pollList.js
+++ src/controllers/pollList.js
@@ -1,7 +1,12 @@
 import {db, ObjectId} from "../database.js";
 
 async function pollList(req,res){
-
-    res.sendStatus(420);
+    try{
+        const polls = await db.collection('polls').find().toArray();
+        res.status(200).send(polls);
+    }catch(error){
+        console.log(error);
+        res.sendStatus(422)
+    }
 }
 export {pollList}
