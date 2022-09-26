38fc1048adea89c679b509ef9bb1b7f60f63b569
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-25 21:51:03 -0300

    show poll choices

--- src/controllers/showPollChoices.js
+++ src/controllers/showPollChoices.js
@@ -1,4 +1,18 @@
+import { db, ObjectId } from "../database.js";
+
 async function showPollChoices(req,res){
-    
+    const {id} = req.params;
+    try{
+        const poll = await db.collection('polls').findOne({_id: ObjectId(id)});
+        if(!poll){
+            return res.sendStatus(404);
+        }
+        
+        const choices = await db.collection('choices').find({pollId: id}).toArray();
+        res.status(200).send(choices)
+    } catch(error){
+        console.log(error);
+        res.sendStatus(422);
+    }
 }
 export {showPollChoices}
