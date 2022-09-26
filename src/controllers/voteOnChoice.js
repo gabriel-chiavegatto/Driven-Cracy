9db17c9caa490ccef5f159d8d1ac049ef1191c5a
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-25 22:15:04 -0300

    vote on choice

---
+++ src/controllers/voteOnChoice.js
@@ -0,0 +1,25 @@
+import {db, ObjectId} from "../database.js";
+import dayjs from 'dayjs';
+async function voteOnChoice(req,res){
+    const {id} = req.params;
+    const hour = dayjs().format("YYYY-MM-DD HH:mm");
+    console.log(hour)
+    try{
+        const choice = await db.collection('choices').findOne({_id: ObjectId(id)});
+        if(!choice){
+            return res.sendStatus(404);
+        }
+
+        const vote = {
+            createdAt: hour,
+            choiceId: ObjectId(id)
+        }
+        await db.collection('votes').insertOne({vote})
+
+        res.sendStatus(201)
+    }catch(error){
+        console.log(error);
+        res.sendStatus(422);
+    }
+}
+export {voteOnChoice}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { addChoice } from "./controllers/addChoice.js";
 import { newPoll } from "./controllers/newPoll.js";
 import { pollList } from "./controllers/pollList.js";
 import { showPollChoices } from "./controllers/showPollChoices.js";
+import { voteOnChoice } from "./controllers/voteOnChoice.js";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post('/poll', newPoll);
 router.get('/poll', pollList);
 router.post('/choice', addChoice);
 router.get('/poll/:id/choice', showPollChoices);
+router.post('/choice/:id/vote', voteOnChoice);
 
 export default router;
